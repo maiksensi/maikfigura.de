d591d0d65b14488fc8a3c256715f6283732e8c33
Maik Figura <maik@maikfigura.de>
2020-06-07 14:19:37 +0200

    Remove generator meta tag from production build

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,3 +7,23 @@ require("typeface-fira-code")
 export const wrapRootElement = ({ element }) => {
   return <Header>{element}</Header>
 }
+
+const isGeneratorTag = (type, name) => type === "meta" && name === "generator"
+
+/**
+ * Remove generator (exposes that this page was
+ * built with gatsby and its version) tag in production builds
+ * @see https://www.gatsbyjs.org/docs/ssr-apis/#onPreRenderHTML
+ */
+export const onPreRenderHTML = ({
+  getHeadComponents,
+  replaceHeadComponents,
+}) => {
+  // filter head component against generator meta tag
+  const headComponents = getHeadComponents().filter(
+    ({ type, props: { name } = {} }) => !isGeneratorTag(type, name)
+  )
+
+  // replace with our filtered component list
+  replaceHeadComponents(headComponents)
+}
