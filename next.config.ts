1f7c978a0e6e3d3f2ea1e160d68ec1437f55c2e0
Maik Figura <maiksensi@gmail.com>
2025-07-24 22:04:59 +0200

    Optimize build

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  // These work with static export
   compress: true,
-  poweredByHeader: false,
+  poweredByHeader: true,
 }
 
 export default nextConfig
